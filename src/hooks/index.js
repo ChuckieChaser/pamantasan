344dd1181863093fb2c337506501a0a94a33900c
Chuckie <chuckie12602@gmail.com>
2026-08-27 17:18:39 +0800

    feat: add hooks index file to export custom hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,22 @@
+// --- IMPORTS & EXPORTS ---
+import useAuth from './useAuth';
+import useClickOutside from './useClickOutside';
+import useDarkMode from './useDarkMode';
+import useDoubleClick from './useDoubleClick';
+import useSmartPosition from './useSmartPosition';
+
+export {
+    useAuth,
+    useClickOutside,
+    useDarkMode,
+    useDoubleClick,
+    useSmartPosition,
+};
+
+export default {
+    useAuth,
+    useClickOutside,
+    useDarkMode,
+    useDoubleClick,
+    useSmartPosition,
+};
